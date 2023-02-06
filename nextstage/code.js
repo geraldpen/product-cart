d1e2a0b08700232bc32722ade0de716a0e855b2e
geraldpen <102095892+geraldpen@users.noreply.github.com>
2023-02-06 15:43:29 +0100

    files
    putting the files

---
+++ nextstage/code.js
@@ -0,0 +1,48 @@
+// // class Person{
+// //   name = 'gerald';
+// //    const myfun =() =>{
+
+// //    }
+
+// // };
+
+// // const person = new Person();
+// // console.log(Person.name);
+// // function brotliCompressSync(){
+// //   console.log("hello whfh");
+// // }
+// // brotliCompressSync();
+
+// const Myfunction= ()=> 'the boss';
+
+// console.log(Myfunction())
+// const myArr = ["min", "yours", "hes"];
+// const myAr2 = ["min", "yours", "hes"];
+// console.log(...myArr ,)
+//  const oldObj = {
+//     id: "nfgfj"
+//  }
+//  const oldObj2 = {
+//     ...oldObj,
+//     name: "nfgfj",
+//     gender: "jerfj"
+//  };
+
+//  console.log(oldObj2)
+
+// const convert = (...args) =>{
+//     return args.map((n)=>  console.log(n))
+    
+// }
+// convert(1, 2, 3);
+
+// const numbers= [1, 2, 3];
+// const [a,b,c] = numbers;
+// console.log(a);
+
+ 
+// const myOjec = {
+//     name
+// }
+
+alert("hbhkbhbjbhj")
